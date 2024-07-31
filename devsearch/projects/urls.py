f551292373e428e29617ed85b651a2b809fed257
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-07-31 07:07:34 +0300

    rendering data to template

--- devsearch/projects/urls.py
+++ devsearch/projects/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . views import project, projects
 
 urlpatterns = [
-    path('', project, name='project'),
-    path('projects/', projects, name='projects')
+    path('', projects, name='projects'),
+    path('project/<str:pk>/', project, name='project')
 ]
