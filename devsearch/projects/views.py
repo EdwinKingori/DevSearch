47b6e3760a502c69b9ba4ef748c040fb7a17b159
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-08-16 16:17:31 +0300

    creating/Post method configuration

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Project, Reviews, Tag
 from .forms import ProjectForm
 # Create your views here.
@@ -21,5 +21,14 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
+  # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = ProjectForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            form.save()
+        # redirect user after filling the form
+        return redirect('projects')
     context = {'form': form}
     return render(request, "projects/project_form.html", context)
