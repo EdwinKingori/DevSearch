bdafffa9a888bea8004d7e827e753b6afba05144
eddmunyiri@gmail.com <eddmunyiri@gmail.com>
2024-09-03 21:21:59 +0300

    add new user app

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'projects',
+    'users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
