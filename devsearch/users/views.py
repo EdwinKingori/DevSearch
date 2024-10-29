e53538edd2670b22c58963e2ca374d0d3f5711a5
Edwin King'ori <eddmunyiri@gmail.com>
2024-10-29 16:41:03 +0300

    implementing flash messages

--- devsearch/users/views.py
+++ devsearch/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from django.contrib.auth.models import User
 from .models import Profile
 # Create your views here.
@@ -14,7 +15,7 @@ def loginPage(request):
         try:
             user = User.objects.get(username=username)
         except:
-            print("Username does not exist!")
+            messages.error(request, "Username does not exist!")
 
         user = authenticate(request, username=username, password=password)
 
@@ -22,13 +23,14 @@ def loginPage(request):
             login(request, user)
             return redirect('profiles')
         else:
-            print("Username or password is incorrect.")
+            messages.error(request, "Username or password is incorrect!")
 
     return render(request, 'users/login_register.html')
 
 
 def logoutUser(request):
     logout(request)
+    messages.success(request, "user was successfuly looged out.")
     return redirect('login')
 
 
