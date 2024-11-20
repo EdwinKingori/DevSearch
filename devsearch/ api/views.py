eb6aab748aa1db4e2601012b2dc6344042f027c7
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-20 15:29:56 +0300

    setting up serializerss & routes

--- devsearch/ api/views.py
+++ devsearch/ api/views.py
@@ -1,6 +1,11 @@
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ProjectSerializer
+from projects.models import Project
 
 
+@api_view(['GET'])
 def getRoutes(request):
 
     routes = [
@@ -14,5 +19,19 @@ def getRoutes(request):
 
     ]
 
-    # safe reurns more than a python dictionary
-    return JsonResponse(routes, safe=False)
+    # safe returns more than a python dictionary
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProjects(request):
+    projects = Project.objects.all()
+    serializer = ProjectSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['GET'])
+def getProject(request, pk):
+    project = Project.objects.get(id=pk)
+    serializer = ProjectSerializer(project, many=False)
+    return Response(serializer.data)
