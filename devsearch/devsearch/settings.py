7e2bdebb780bc2af746fca191557e4a0e206eb02
Edwin King'ori <eddmunyiri@gmail.com>
2024-11-25 20:14:38 +0300

    getting data from the frontend & CORS

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap4',
 
     'rest_framework',
+    "corsheaders",
 ]
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
@@ -46,8 +47,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=5),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=90),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=30),
     "ROTATE_REFRESH_TOKENS": False,
     "BLACKLIST_AFTER_ROTATION": False,
     "UPDATE_LAST_LOGIN": False,
@@ -85,6 +86,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -158,6 +160,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
+
+# a settings configuration to allow other origins to make a cross-site http requests or access your api resources
+CORS_ALLOW_ALL_ORIGINS = True
 
 # configuring email backend
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
